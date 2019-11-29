09a4eed28021860b20fc895b2bcf791187aed96d
Nina <nina.gs20@yandex.ru>
2019-11-29 17:18:43 +0400

    Updates

---
+++ precache-manifest.636fab2c574956cd70f37e145cb9acc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c34469afcae1377704f7360c7d26ceac",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "50ada715fa88f723f7a7",
+    "url": "/weather-app/static/css/main.d96dd74c.chunk.css"
+  },
+  {
+    "revision": "c2faad0ba2c6cce32ba9",
+    "url": "/weather-app/static/js/2.b4ec89fb.chunk.js"
+  },
+  {
+    "revision": "50ada715fa88f723f7a7",
+    "url": "/weather-app/static/js/main.2ec55806.chunk.js"
+  },
+  {
+    "revision": "ad4d9480dbc2856353a6",
+    "url": "/weather-app/static/js/runtime-main.696cfe50.js"
+  },
+  {
+    "revision": "f5d4bf02d6e5f2708f7d5aa520a9d979",
+    "url": "/weather-app/static/media/img.f5d4bf02.jpg"
+  }
+]);
